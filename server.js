a004acd653932f60ce6d5e2cf904620920b45863
Howchmo <howardmall+github@howardmall.net>
2016-09-15 11:46:55 -0400

    This query will now handle cases where the parent title falls behind its children in alphabetical order.

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ function getProjections( req, res, next )
 
 function getProjects( req, res, next )
 {
-	var queryString = "select project, parent, status, title, chargenumber, start, end, manager, customer from (select p1.*, case when p2.parent is null then p2.title else p2.title end as parentTitle from projects p1, projects p2 where p1.parent = p2.project) p  order by parentTitle, title, status desc;";
+	var queryString = "select project, parent, status, title, chargenumber, start, end, manager, customer from (select p1.*, case when p2.parent is null then p2.title else p2.title end as parentTitle from projects p1, projects p2 where p1.parent = p2.project) p  order by parentTitle, status desc, title;";
 	db.query(queryString).then(
 		function( data )
 		{
